0c40b59ffb4982b104c785cfeaa2a0b3338e86a5
Maccesch <maccesch@synphonyte.com>
2023-02-21 12:12:29 -0500

    ready for publishing

--- README.md
+++ README.md
@@ -6,3 +6,11 @@ This is meant to be used in conjunction with the [PANIM Blender Exporter](https:
 Please also check there to see the details of the binary file format.
 
 The primary use case for this file type is to export more animation data on top of what can be stored in GLTF files.
+
+## Usage
+
+```rust
+use panim_loader::PropertiesAnimation;
+
+let anim = PropertiesAnimation::from_file("assets/example.panim").unwrap()
+```
--- src/lib.rs
+++ src/lib.rs
@@ -1,29 +1,46 @@
 pub mod errors;
 mod parser;
 
+/// A Properties Animation file containing all the animations for all exported properties of all objects in the scene.
 #[derive(Clone, Debug, PartialEq)]
 pub struct PropertiesAnimation {
+    /// The number of frames per second.
     pub fps: f32,
+
+    /// The list of animations.
     pub animations: Vec<Animation>,
 }
 
+/// An animation for a single property of a single object.
 #[derive(Clone, Debug, PartialEq)]
 pub struct Animation {
+    /// The unique name of the object.
     pub object_name: String,
+
+    /// The name of the property.
     pub property_name: String,
+
+    /// The first frame of the animation.
     pub frame_start: u32,
+
+    /// The last frame of the animation (including).
     pub frame_end: u32,
+
+    /// The list of values for each frame starting with `frame_start` and ending with `frame_end`.
     pub frame_values: Vec<f32>,
 }
 
+/// The result type for parsing a Properties Animation file.
 type Result = std::result::Result<PropertiesAnimation, errors::Error>;
 
 impl PropertiesAnimation {
+    /// Parses a Properties Animation file from a byte slice.
     pub fn from_bytes(bytes: &[u8]) -> Result {
         let props_animation = parser::props_animation(bytes)?;
         Ok(props_animation.into())
     }
 
+    /// Parses a Properties Animation file from a file path.
     pub fn from_file(path: &str) -> Result {
         let bytes = std::fs::read(path)?;
         Self::from_bytes(&bytes)
